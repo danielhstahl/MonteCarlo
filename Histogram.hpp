1d1b107c0c85fd81b698c8791ce8006450c68699
phillyfan1138 <danstahl1138@gmail.com>
2016-02-05 14:55:57 -0500

    added io enhancements

--- Histogram.hpp
+++ Histogram.hpp
@@ -3,7 +3,7 @@ std::vector<int> bin(int numBins, const Number& min, const Number& max, const st
     int n=data.size();
     std::vector<int> bins(numBins, 0);
     double binWidth=(max-min)/numBins;
-    
+
     for(int i=0; i<n; ++i){
         bins[(int)(data[i]-min)/binWidth]++;
     }
@@ -15,12 +15,12 @@ void binAndSend(int numBins, const Number& min, const Number& max,  const std::v
     std::vector<int> bins(numBins, 0);
     double binWidth=(max-min)/numBins;
     double orderMagnitude=max-min;
-    
+
     orderMagnitude=pow(10.0, 2-(int)log10(orderMagnitude));
     for(int i=0; i<n; ++i){
         bins[(int)((data[i]-min)/binWidth)]++;
     }
-    
+
     std::cout<<"{\"bins\":[";
     for(int i=0; i<(numBins-1); ++i){
         std::cout<<"\""<<((int)((min+i*binWidth)*orderMagnitude))/orderMagnitude<<"-"<<((int)((min+(i+1)*binWidth)*orderMagnitude))/orderMagnitude<<"\",";
@@ -29,7 +29,7 @@ void binAndSend(int numBins, const Number& min, const Number& max,  const std::v
     for(int i=0; i<(numBins-1); ++i){
         std::cout<<bins[i]<<",";
     }
-    std::cout<<bins[numBins-1]<<"]}"<<std::endl;
+    std::cout<<bins[numBins-1]<<"]}\\n"<<std::endl;
 }
 /*template<typename Number>
 std::vector<int> bin(int numBins, const std::vector<Number>& data){
@@ -59,5 +59,5 @@ void binAndSend(Number& min, Number& max, const std::vector<Number>& data){
     }
     int numBins=2*pow(n, 1.0/3.0);//Rice Rule
     binAndSend(numBins, min, max, data);
-    
+
 }
